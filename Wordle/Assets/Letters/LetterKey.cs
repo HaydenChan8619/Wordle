bed940ab24501f6bcf7c51c2256f79157e0f5712
HaydenChan8619 <haydenphchan@gmail.com>
2023-08-16 10:19:21 -0700

    Keyboard Added
    Added the keyboard to the game, with all letters added with null functionality; keyboard missing enter and backspace

---
+++ Wordle/Assets/Letters/LetterKey.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+This class represents what happens when a key on the keyboard gets clicked
+*/
+
+public class LetterKey : MonoBehaviour
+{
+    public char letter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public void click() {
+        
+    }
+}
