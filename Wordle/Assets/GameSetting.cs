edcc8065058684b83aa47ad7b8ccd92fca73f1eb
HaydenChan8619 <haydenphchan@gmail.com>
2023-08-18 17:23:40 -0700

    Start Menu Completed
    Made a start menu that would trigger the moment the game is launched

--- Wordle/Assets/GameSetting.cs
+++ Wordle/Assets/GameSetting.cs
@@ -7,11 +7,24 @@ public class GameSetting : MonoBehaviour
     public GameState game;
     public AnswerKey answer;
     public GameObject gameplayCanvas;
+    public GameObject startCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameplayCanvas.SetActive(false);
+        startCanvas.SetActive(true);
+    }
 
     // EFFECTS: starts a new game
     public void newGame() {
         answer.setAnswerKey();
         game.resetAllBackgrounds();
+        canvasReset();
+    }
+
+    public void canvasReset() {
         gameplayCanvas.SetActive(true);
+        startCanvas.SetActive(false);
     }
 }
