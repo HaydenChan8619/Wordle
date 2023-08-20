165c3c52a5fa7595738bc8b3007e63e98009e0fe
HaydenChan8619 <haydenphchan@gmail.com>
2023-08-19 21:45:44 -0700

    Added full Keycode implementation
    Fixed the return key bug by setting the button's navigation to "none"

--- Wordle/Assets/Letters/KeyBindings.cs
+++ Wordle/Assets/Letters/KeyBindings.cs
@@ -9,31 +9,34 @@ public class KeyBindings : MonoBehaviour
 
     // MODIFIES: this
     // EFFECTS: initializes the keyCodeDictionary
-    private void Awake()
-    {
+    private void Awake() {
         InitializekeyCodeDictionary();
     }
 
-    private void Update()
-    {
-        if (Input.anyKeyDown)
-        {
-            foreach (KeyCode keyCode in keyCodeDictionary.Keys)
-            {
-                if (Input.GetKeyDown(keyCode))
-                {
-                    char character = keyCodeDictionary[keyCode];
-                    game.addLetter(character);
-                }
+    private void Update() {
+        if (Input.anyKeyDown && game.isGameActive) {            
+            
+            if (Input.GetKeyDown(KeyCode.Return)) {
+                Debug.Log("Return key pressed");
+                game.confirmAnswer();
+            } else if (Input.GetKeyDown(KeyCode.Backspace)) {
+                Debug.Log("Backspace key pressed");
+                game.removeLast();
+            } else {
+                foreach (KeyCode keyCode in keyCodeDictionary.Keys) {
+            
+                    if (Input.GetKeyDown(keyCode)) {
+                        char character = keyCodeDictionary[keyCode];
+                        game.addLetter(character);
+                    }
+                }   
             }
         }
     }
 
     // EFFECTS: initializes the keyCodeDictionary by looping through all alphabet KeyCodes and assigning their corresponding character
-    private void InitializekeyCodeDictionary()
-    {
-        for (KeyCode keyCode = KeyCode.A; keyCode <= KeyCode.Z; keyCode++)
-        {
+    private void InitializekeyCodeDictionary() {
+        for (KeyCode keyCode = KeyCode.A; keyCode <= KeyCode.Z; keyCode++) {
             keyCodeDictionary[keyCode] = (char)keyCode;
         }
     }
